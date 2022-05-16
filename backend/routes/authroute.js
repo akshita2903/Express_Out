56a377d5c3fa3dfff045ffc122cd38498c4a59d4
akshita2903 <akshita29320@gmail.com>
2022-05-16 12:58:46 +0530

    route for reset password

--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,55 +1,37 @@
 const express=require('express');
 const asyncHandler=require('express-async-handler');
 const router=express.Router();
+const bcrypt=require('bcryptjs');
 const User=require('../models/userModel');
 const generateToken = require('../util/generateToken');
 //router.use(cookie);
 router.post('/signup',asyncHandler(async(req,res)=>{
 
-
-
-    const{name,email,password}=req.body
-    console.log(req.body)
-    try{
-
-    const doesexist=await User.findOne({email});
-    if(doesexist){
+const{name,email,password}=req.body;
+const userExist=await User.findOne({email});
+if(userExist)
+{
+    console.log("Already Exist");
+    res.status(201).send("User Already Exist");
+}
+else{
+    const user=await User.create({
+        name,
+        email,
+        password
+    });
+    if(user){
         res.status(200).json({
-            status:300,
-           // message:"Email already exist"
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token:generateToken(user._id),
         })
-      //  throw new Error("User already Exists");
     }
     else{
-        const user=await User.create({
-            name,email,password
-        });
-        if(user) {
-            res.status(201).json({
-                status:200,
-                _id:user._id,
-                name:user.name,
-                email:user.email,
-                token:generateToken(user._id),
-               // message:"Sign Up Successfully"
-            })
-        }
-        else{
-            res.status(200).json({
-                status:400,
-                message:"Some Error occured"
-            })
-           // throw new Error("Error occured")
-        }
-       res.json({
-           name,email
-       })
+        res.status(400).send("User not Found");
     }
-
-    }
-    catch(err){
-        console.log(err)
-    }
+}
 }));
 router.post('/login',asyncHandler(async(req,res)=>{
 const {email,password}=req.body;
@@ -58,40 +40,67 @@ try{
 const user=await User.findOne({email});
 //console.log(user);
 const validP=await user.matchPassword(password);
-console.log(validP);
 
 
-if(user  && validP){
-    res.status(200).json({
-        status:200,
-        _id:user._id,
-        name:user.name,
-        email:user.email,
-        token:generateToken(user._id),
-        message:"Login Successfully"
-        
-    });
+if( !user || !validP){
+    
+      res.status(201).send("Incorrect Details")
   //  windows.alert("Logged In successfully");
 }
-else {
-    res.status(200).json({
-        status:300
-    });
+
+    else{
+        res.status(200).json({
+            status:200,
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token:generateToken(user._id),
+            
+            
+        });
+    }
 //throw new Error("Errorrrr")
-}
+
 }
 catch(err){
-    console.log(err);
+    
+    res.status(400).send("Try After Sometime");
 }
 }));
-//Update
-// router.put('/:id',async(req,res)=>{
-//     const user= await User.findById(req.user._id);
-//     if(user){
-//         user.name=req.body.name || user.name;
-//         user.email
-//     }
-// })
+//Update Password
+ router.put('/changePassW',async(req,res)=>{
+    const{email}=req.body;
+    
+    const user=await User.findOne({email});
+    if(!user){
+        res.status(201).send("Email Not exist");
+    }
+   else{
+       console.log(user._id);
+if(req.body.password){
+   user.password=req.body.password;
+}
+    try{
+// console.log(password)
+console.log("Before saving");
+
+
+const updatedUser=await user.save();
+res.status(200).json({
+    _id:updatedUser._id,
+    email:updatedUser.email,
+    token:generateToken(updatedUser._id),
+});
+    }
+
+    
+catch(err){
+    console.log(err+" error")
+res.status(401).send("Something Went Wrong!!");
+}
+ }
+}
+ );
 
 router.post('/logOut')
 module.exports=router;
