bde3f21f91d411464da5fbffbc39e88d4ea80c9d
akshita2903 <akshita29320@gmail.com>
2022-05-23 11:53:42 +0530

    design

--- frontEnd/src/Components/HomePage/Post_daalne_or_show_walaPage.js
+++ frontEnd/src/Components/HomePage/Post_daalne_or_show_walaPage.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 import {Box, Typography,makeStyles} from '@material-ui/core'
 
 const useStyle=makeStyles({
 box_design:{
-  margin:20,
+  margin:10,
   borderRadius:10,
   border:'1px solid #d3cede',
   display:'flex',
   alignItems:'center',
   flexDirection:'column',
-   height: 'auto',
-        
+   height: 350, 
+  
     },
 image:{
    height:150, 
   width:'100%',
   objectFit:'cover',
-  borderRadius:10
+  borderRadius:10,
   border:'1px solid #d3cede'
 },
 text:{
@@ -26,19 +26,24 @@ text:{
 },
 title:{
   fontSize:18,
-  fontWeight:350
+  fontWeight:550
 },
 desc:{
   fontSize:14,
   wordBreak:'break-word'
 }
-})
+});
 
 export default function Post_daalne_or_show_walaPage(props) {
   //backend to front_end
 
   const classes=useStyle();
  const url='https://primarywall.com/wp-content/uploads/2021/03/5082979-1500x1000.jpeg'
+ 
+const addEllips=(str,limit)=>{
+  let len=str.length;
+  return len >limit ?str.substring(0,limit)+ '...' :str;
+}
 
   return (
   
@@ -47,7 +52,7 @@ export default function Post_daalne_or_show_walaPage(props) {
    
     <Typography className={classes.title}>{props.title}</Typography>
     <Typography className={classes.text}>{props.name}</Typography>
-    <Typography className={classes.desc}>{props.description}</Typography>
+    <Typography className={classes.desc}>{addEllips(props.description,300)}</Typography>
 
    </Box>
    
